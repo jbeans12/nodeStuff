e4e89b31be501941f7570929808f38de4a239d9b
sharon <martinezsharonr@gmail.com>
2021-03-05 12:21:10 -0800

    added template literals in javascript file

--- index.js
+++ index.js
@@ -2,17 +2,16 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 
 
-const html = createHTML(
     inquirer
   .prompt([
     {
       type: 'input',
-      name: 'profile name',
+      name: 'profilename',
       message: 'What is your name?',
     },
     {
       type: 'input',
-      name: 'location place',
+      name: 'location',
       message: 'What is your location?',
     },
     {
@@ -21,13 +20,33 @@ const html = createHTML(
         message: 'What is your LinkedIn profile name?',
     },
   ])
-)
-
-
   .then((data) => {
+    console.log(data)
     
+    var html = `<!DOCTYPE html>
+    <html lang="en">
+    <head>
+      <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Document</title>
+    </head>
+    <body>
 
+      <div id="profile">
+        <h2>${data.profilename}</h2>
+        <p> ${data.location}</p>
+        <p> ${data.url}</p>
+      </div>
+      
+      <script type="text/javascript" src="index.js"></script>
+    </body>
+    </html>`;
+  
     fs.writeFile('index.html', html, function (err) {
         if (err) console.log(err)
       })
   });
+
+
+ 
